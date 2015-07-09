580799f16158024a55ca0e069fda92e781c94730
Mandy Chan <mchan9993@gmail.com>
2015-07-09 10:58:33 +0000

    Functional Utterance Log Update Script

--- UtteranceLogUpdate.sh
+++ UtteranceLogUpdate.sh
@@ -2,6 +2,17 @@
 # Utterance Log Archival System
 #
 
-echo "Going to transcription_tool_common user"
-echo "transcription" | sudo -u transcription_tool_common
-echo "Success"
+#Gets today's date
+today=$(date +%Y-%m-%d)
+
+echo "Switching user to transcription_tool_common"
+
+# This is kind of unsafe since password is exposed
+# Goes into transcription_tool_common's home/logs folder
+# Copies log.sql to another file and clears the content of log.sql
+echo "mchan18221" | sudo -kS -u transcription_tool_common -H sh -c "cd ;
+cd logs ;
+cp log.sql log-$today.sql ;
+> log.sql
+"
+echo "Utterance Log Archived for $today"
