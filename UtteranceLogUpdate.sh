ddeaca4f0488688d8f0899d6f691dd6975cc9a50
Mandy Chan <mchan9993@gmail.com>
2015-07-13 17:45:30 +0000

    Utterance Log finalized (used in tt_common)

--- UtteranceLogUpdate.sh
+++ UtteranceLogUpdate.sh
@@ -5,14 +5,10 @@
 #Gets today's date
 today=$(date +%Y-%m-%d)
 
-echo "Switching user to transcription_tool_common"
-
-# This is kind of unsafe since password is exposed
 # Goes into transcription_tool_common's home/logs folder
 # Copies log.sql to another file and clears the content of log.sql
-echo "mchan18221" | sudo -kS -u transcription_tool_common -H sh -c "cd ;
-cd logs ;
-cp log.sql log-$today.sql ;
-> log.sql
-"
+cd logs
+cp sql.log UtterLog-$today.sql
+> sql.log
+
 echo "Utterance Log Archived for $today"
