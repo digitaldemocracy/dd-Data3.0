6d25fba392dd4ded8b1e65034baca0b9facf2590
John Alkire <jalkire@calpoly.edu>
2016-01-08 21:12:57 -0800

    Updated NY scripts
    Should be working

--- Python_Scripts/NY-Build/import_committees_ny.py
+++ Python_Scripts/NY-Build/import_committees_ny.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 '''
 File: import_committees_ny.py
 Author: John Alkire
@@ -11,101 +12,148 @@ import requests
 import MySQLdb
 
 def call_senate_api(restCall, year, house, offset):
-	if house != "":
-		house = "/" + house
-	url = "http://legislation.nysenate.gov/api/3/" + restCall + "/" + str(year) + house + "?full=true&limit=1000&key=31kNDZZMhlEjCOV8zkBG1crgWAGxwDIS&offset=" + str(offset)
-	r = requests.get(url)
-	out = r.json()
-	return out["result"]["items"]
+    if house != "":
+        house = "/" + house
+    url = "http://legislation.nysenate.gov/api/3/" + restCall + "/" + str(year) + house + "?full=true&limit=1000&key=31kNDZZMhlEjCOV8zkBG1crgWAGxwDIS&offset=" + str(offset)
+    r = requests.get(url)
+    out = r.json()
+    return out["result"]["items"]
 
 def get_last_cid_db(dddb):
-	select_comm = '''SELECT cid FROM Committee
-					ORDER BY cid DESC
-					LIMIT 1
+    select_comm = '''SELECT cid FROM Committee
+                    ORDER BY cid DESC
+                    LIMIT 1
                   '''
-	dddb.execute(select_comm)
-	
-	query = dddb.fetchone();
-	return query[0]
+    dddb.execute(select_comm)
+    
+    query = dddb.fetchone();
+    return query[0]
 
+def clean_name(name):
+    problem_names = {
+        "Inez Barron":("Charles", "Barron"), 
+        "Philip Ramos":("Phil", "Ramos"), 
+        "Thomas McKevitt":("Tom", "McKevitt"), 
+        "Albert Stirpe":("Al","Stirpe"), 
+        "Peter Abbate":("Peter","Abbate, Jr."),
+        "Sam Roberts":("Pamela","Hunter"),
+        "Herman Farrell":("Herman", "Farrell, Jr."),
+        "Fred Thiele":("Fred", "Thiele, Jr."),
+        "William Scarborough":("Alicia", "Hyndman"),
+        "Robert Oaks":("Bob", "Oaks"),
+        "Andrew Goodell":("Andy", "Goodell"),
+        "Peter Rivera":("José", "Rivera"),
+        "Addie Jenne Russell":("Addie","Russell"),
+        "Kenneth Blankenbush":("Ken","Blankenbush"),
+        "Alec Brook-Krasny":("Pamela","Harris"),
+        "Mickey Kearns":("Michael", "Kearns"),
+        "Steven Englebright":("Steve", "Englebright"),
+        
+    }
+    ending = {'Jr':', Jr.','Sr':', Sr.','II':' II','III':' III', 'IV':' IV'}
+    name = name.replace(',', ' ')
+    name = name.replace('.', ' ')
+    name = name.replace('  ', ' ')
+    name_arr = name.split()      
+    suffix = "";               
+    for word in name_arr:
+        if word != name_arr[0] and (len(word) <= 1 or word in ending.keys()):
+            name_arr.remove(word)
+            if word in ending.keys():
+                suffix = ending[word]            
+            
+    first = name_arr.pop(0)
+    while len(name_arr) > 1:
+        first = first + ' ' + name_arr.pop(0)            
+    last = name_arr[0]
+    last = last.replace(' ' ,'') + suffix
+    
+    if (first + ' ' + last) in problem_names.keys():             
+        return problem_names[(first + ' ' + last)]
+        
+    return (first, last)
+    
 def get_committees_api(year):
-	committees = call_senate_api("committees", 2015, "senate", 0)
-	ret_comms = list()
-	for comm in committees:
-		committee = dict()
-		committee['name'] = comm['name']
-		committee['type'] = "Standing"
-		committee['house'] = "Senate"
-		committee['state'] = "NY"
-		committee['members'] = list()
-		members = comm['committeeMembers']['items']
-		for member in members:
-			sen = dict()		
-			name = member['fullName'].split() 		
-			sen['last'] = name[len(name) - 1]		
-			sen['first'] = name[0]
-			for x in range(1, len(name) - 1):
-				sen['first'] = sen['first'] + " " + name[x]
-			sen['year'] = str(year)
-			sen['house'] = "Senate"
-			sen['state'] = "NY"
-			committee['members'].append(sen)
-		ret_comms.append(committee)
-	return ret_comms
+    committees = call_senate_api("committees", 2015, "senate", 0)
+    ret_comms = list()
+    for comm in committees:
+        committee = dict()
+        committee['name'] = comm['name']
+        committee['type'] = "Standing"
+        committee['house'] = "Senate"
+        committee['state'] = "NY"
+        committee['members'] = list()
+        members = comm['committeeMembers']['items']
+        for member in members:
+            sen = dict()                
+            name = clean_name(member['fullName']) 
+            sen['last'] = name[1]
+            sen['first'] = name[0]
+            sen['year'] = str(year)
+            sen['house'] = "Senate"
+            sen['state'] = "NY"
+            if member['title'] == "CHAIR_PERSON":           
+                sen['position'] = "chair"
+            else:
+                sen['position'] = "member"
+            committee['members'].append(sen)
+        ret_comms.append(committee)
+    return ret_comms
 
 def add_committees_db(year, dddb):
-	committees = get_committees_api(year)
+    committees = get_committees_api(year)
 
-	for committee in committees:
-		cid = get_last_cid_db(dddb) + 1
-		committee['cid'] = str(cid)
-		insert_stmt = '''INSERT INTO Committee
-						(cid, house, name, type, state)
-						VALUES
-						(%(cid)s, %(house)s, %(name)s, %(type)s, %(state)s);
-						'''
-		print (insert_stmt % committee)
-		dddb.execute(insert_stmt, committee)
-	
-		x = 0
-		for member in committee['members']:
-			member['pid'] = get_pid_db(member['first'], member['last'], dddb)
-			member['cid'] = committee['cid']
-			insert_stmt = '''INSERT INTO servesOn
-						(pid, year, house, cid, state)
-						VALUES
-						(%(pid)s, %(year)s, %(house)s, %(cid)s, %(state)s);
-						'''
-			print (insert_stmt % member)
-			dddb.execute(insert_stmt, member)
-			x = x + 1
-		print x
-						
-	
+    for committee in committees:
+        cid = get_last_cid_db(dddb) + 1
+        committee['cid'] = str(cid)
+        insert_stmt = '''INSERT INTO Committee
+                        (cid, house, name, type, state)
+                        VALUES
+                        (%(cid)s, %(house)s, %(name)s, %(type)s, %(state)s);
+                        '''
+        print (insert_stmt % committee)
+        dddb.execute(insert_stmt, committee)
+    
+        x = 0
+        for member in committee['members']:
+            member['pid'] = get_pid_db(member['first'], member['last'], dddb)
+            member['cid'] = committee['cid']
+            insert_stmt = '''INSERT INTO servesOn
+                        (pid, year, house, cid, state)
+                        VALUES
+                        (%(pid)s, %(year)s, %(house)s, %(cid)s, %(state)s);
+                        '''
+            print (insert_stmt % member)
+            dddb.execute(insert_stmt, member)
+            x = x + 1
+        print x
+                        
+    
 def get_pid_db(first, last, dddb):
-	select_person = '''SELECT * FROM Person
+    select_person = '''SELECT * FROM Person
                      WHERE first = %(first)s
                       AND last = %(last)s 
                   '''
-	dddb.execute(select_person, {'first':first,'last':last})
-	#print (select_person %  {'first':first,'last':last})
-	query = dddb.fetchone();
-	return query[0]
-	
+    dddb.execute(select_person, {'first':first,'last':last})
+    #print (select_person %  {'first':first,'last':last})
+    query = dddb.fetchone();
+    return query[0]
+    
 
 def main():
-	dddb_conn =  MySQLdb.connect(host='digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com',
-						user='awsDB',
-						db='JohnTest',
-						port=3306,
-						passwd='digitaldemocracy789')
-	dddb = dddb_conn.cursor()
-	dddb_conn.autocommit(True)
+    dddb_conn =  MySQLdb.connect(host='digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com',
+                        user='awsDB',
+                        db='JohnTest',
+                        port=3306,
+                        passwd='digitaldemocracy789',
+                        charset='utf8')
+    dddb = dddb_conn.cursor()
+    dddb_conn.autocommit(True)
 
-	add_committees_db(2015, dddb)
-	
-	dddb_conn.close()
+    add_committees_db(2015, dddb)
+    
+    dddb_conn.close()
 main()
-	
-	
+    
+    
 
--- Python_Scripts/NY-Build/import_committees_scrape_assembly_ny.py
+++ Python_Scripts/NY-Build/import_committees_scrape_assembly_ny.py
@@ -1,3 +1,12 @@
+'''
+File: import_committees_ny.py
+Author: John Alkire
+Date: 12/16/2015
+Description:
+- Imports NY committees by scraping assembly webpage
+- Fills Committee and servesOn
+- Currently configured to test DB
+'''
 from lxml import html
 import requests
 import MySQLdb
@@ -13,71 +22,82 @@ def get_last_cid_db(dddb):
 	return query[0]
 
 def clean_name(name):
-    bad = ['Jr','Sr','II','III', 'IV']
-    name = name.replace(',', '')
-    name = name.replace('.', '')
-    name_arr = name.split()                     
+    ending = {'Jr':', Jr.','Sr':', Sr.','II':' II','III':' III', 'IV':' IV'}
+    name = name.replace(',', ' ')
+    name = name.replace('.', ' ')
+    name_arr = name.split()      
+    suffix = "";               
     for word in name_arr:
-        if len(word) <= 1 or  word in bad:
+        if word != name_arr[0] and (len(word) <= 1 or word in ending.keys()):
             name_arr.remove(word)
-    first = name_arr[0]
-    last = name_arr[1]
-    for x in range(2, len(name_arr)):
-        last = last + ' ' + name_arr[x]    
+            if word in ending.keys():
+                suffix = ending[word]            
+            
+    first = name_arr.pop(0)
+    while len(name_arr) > 1:
+        first = first + ' ' + name_arr.pop(0)            
+    last = name_arr[0] + suffix
     return (first, last)
     
 def get_committees_html(year):
     page = requests.get('http://assembly.state.ny.us/comm/')
     tree = html.fromstring(page.content)
-
     categories_html = tree.xpath('//*[@id="sitelinks"]/span//text()')
     ret_comms = list()
     committees = dict()
     x = 1
+    positions = ["chair", "member"]
+    count = 0;
     for category in categories_html:
         committees_html = tree.xpath('//*[@id="sitelinks"]//ul['+str(x)+']//li/strong/text()')        
         y = 1
+        print category
         for comm in committees_html:                    
             link = tree.xpath('//*[@id="sitelinks"]//ul['+str(x)+']//li['+str(y)+']/a[contains(@href,"mem")]/@href')
+            committee = dict()
+            committee['name'] = comm
+            committee['type'] = category
+            committee['house'] = "Assembly"
+            committee['state'] = "NY"
+            committee['members'] = list()
+            print "\t"+comm
+            
             if len(link) > 0:
                 strip_link = link[0][0:len(link[0]) - 1]
             
                 link = 'http://assembly.state.ny.us/comm/' + strip_link
-                committee = dict()
-            
-                committee['members'] = list()
+                           
+           
                 
                 member_page = requests.get(link)
                 member_tree = html.fromstring(member_page.content)
                 
                 members_html = member_tree.xpath('//*[@id="sitelinks"]/span//li/a//text()')
-                
+                position = 0
                 for mem in members_html:                    
                     sen = dict()
                     name = clean_name(mem)                        
-                    
+                    sen['position'] = positions[position] 
                     sen['last'] = name[1]        
                     sen['first'] = name[0]                                        
                     sen['year'] = str(year)
                     sen['house'] = "Assembly"
                     sen['state'] = "NY"
                     committee['members'].append(sen)
+                    position = 1
 
-                committee['name'] = comm
-                committee['type'] = category
-                committee['house'] = "Assembly"
-                committee['state'] = "NY"
+            
+            count = count + 1
             ret_comms.append(committee)    
             y = y + 1
-        x = x + 1
-           # print members_html
-    return ret_comms 
-        
-        
+        x = x + 1   
+    print "Count: " + str(count) + "Len: " + str(len(ret_comms))       
+    return ret_comms                 
 
 def add_committees_db(year, dddb):
     committees = get_committees_html(year)
-
+    print "Len2: " + str(len(committees))
+    count = 0
     for committee in committees:
         cid = get_last_cid_db(dddb) + 1
         committee['cid'] = str(cid)
@@ -86,26 +106,24 @@ def add_committees_db(year, dddb):
                         VALUES
                         (%(cid)s, %(house)s, %(name)s, %(type)s, %(state)s);
                         '''
-        print (insert_stmt % committee)
+        print committee['name']
         dddb.execute(insert_stmt, committee)
-    
-        x = 0
+        count = count + 1
         if len(committee['members']) > 0:
             for member in committee['members']:
                 member['pid'] = get_pid_db(member, dddb)
                 member['cid'] = committee['cid']
                 insert_stmt = '''INSERT INTO servesOn
-                            (pid, year, house, cid, state)
+                            (pid, year, house, cid, state, position)
                             VALUES
-                            (%(pid)s, %(year)s, %(house)s, %(cid)s, %(state)s);
+                            (%(pid)s, %(year)s, %(house)s, %(cid)s, %(state)s, %(position)s);
                             '''
+                
                 if member['pid'] != "bad":
-                    #print (insert_stmt % member)
                     dddb.execute(insert_stmt, member)
-                x = x + 1
-      
-                        
-    
+    print "Final: " + str(count)
+                
+
 def get_pid_db(person, dddb):
     select_person = '''SELECT * 
                        FROM Person p, Legislator l
@@ -114,7 +132,7 @@ def get_pid_db(person, dddb):
     
     try:
         dddb.execute(select_person, person)
-        query = dddb.fetchone();
+        query = dddb.fetchone()
         return query[0]
     except:
         print "Error", (select_person %  person)
