5a49696ba2df5f2e0df54649ca3c7096afccb2cf
anvo268 <andrewvoorhees@gmail.com>
2016-01-20 10:56:17 -0800

    Updated the database to Dec

--- Python_Scripts/UsedForUpdate/BuildAndrewTables.sh
+++ Python_Scripts/UsedForUpdate/BuildAndrewTables.sh
@@ -1,2 +1,2 @@
-mysql -h digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com -P 3306 -u awsDB -pdigitaldemocracy789 DDDB2015July < OrgAlignments.sql
-mysql -h digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com -P 3306 -u awsDB -pdigitaldemocracy789 DDDB2015July < LegParticipation.sql
+mysql -h digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com -P 3306 -u awsDB -pdigitaldemocracy789 DDDB2015Dec < OrgAlignments.sql
+mysql -h digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com -P 3306 -u awsDB -pdigitaldemocracy789 DDDB2015Dec < LegParticipation.sql
