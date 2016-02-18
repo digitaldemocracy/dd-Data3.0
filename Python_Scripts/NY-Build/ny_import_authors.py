6ddc0f0c123a444bc13c48d32c695f1a2dc81cc6
roh <rohroh94@gmail.com>
2016-02-17 18:13:15 -0800

    putting changes back

--- Python_Scripts/NY-Build/ny_import_authors.py
+++ Python_Scripts/NY-Build/ny_import_authors.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 # -*- coding: utf8 -*-
 '''
-File: ny_import_authors.py
+File: import_authors_ny.py
 Author: Min Eric Roh
 Date: 12/26/2015
 Description:
@@ -11,13 +12,49 @@ Description:
 '''
 import requests
 import MySQLdb
+import loggingdb
 
-counter = 0;
+counter = 0
+
+US_STATE = 'NY'
+
+# URL
+URL = ('http://legislation.nysenate.gov/api/3/%(restCall)s/%(year)s%(house)s' +
+		'?full=true&limit=1000&key=IhV5AXQ1rhUS8ePXkfwsO4AvjQSodd4Q&offset=%(offset)s')
+
+# INSERTS
+QI_AUTHORS = '''	INSERT INTO authors
+						(pid, bid, vid, contribution)
+						VALUES
+						(%(pid)s, %(bid)s, %(vid)s, %(contribution)s)'''
+
+# SELECTS
+QS_AUTHORS_CHECK = ''' 	SELECT pid
+						FROM authors
+						WHERE bid = %(bid)s
+						 AND vid = %(vid)s
+						 AND contribution = %(contribution)s'''
+QS_PERSON = '''	SELECT pid
+	            FROM Person
+	            WHERE last = %s
+	             AND first = %s
+	            ORDER BY Person.pid'''
+QS_BILL = '''	SELECT * FROM Bill
+				WHERE bid = %s'''
+QS_BILLVERSION = '''	SELECT * FROM BillVersion
+						WHERE vid = %s'''
+
+# UPDATE
+QU_AUTHORS = '''	UPDATE authors
+					SET pid = %(pid)s
+					WHERE bid = %(bid)s
+					 AND vid = %(vid)s
+					 AND contribution = %(contribution)s'''
 
 def call_senate_api(restCall, year, house, offset):
 	if house != "":
 		house = "/" + house
-	url = "http://legislation.nysenate.gov/api/3/" + restCall + "/" + str(year) + house + "?full=true&limit=1000&key=IhV5AXQ1rhUS8ePXkfwsO4AvjQSodd4Q&offset=" + str(offset)
+	url = URL % {'restCall':restCall, 'year':str(year), 'house':house, 'offset':str(offset)}
 	r = requests.get(url)
 	print url
 	out = r.json()
@@ -33,7 +70,7 @@ def get_author_api(year):
 		bills = call[0]
 		total = call[1]
 		for bill in bills:
-			if bill['sponsor']['member'] is not None and len(bill['sponsor']['member']['fullName'].split()) > 1:
+			if bill['sponsor']['member'] is not None:
 				b = dict()
 				b['type'] = bill['basePrintNo']
 #				print b['type']
@@ -51,41 +88,32 @@ def get_author_api(year):
 
 def insert_authors_db(bill, dddb):
 	global counter
-	insert_stmt = 	'''	INSERT INTO authors
-						(pid, bid, vid, contribution)
-						VALUES
-						(%(pid)s, %(bid)s, %(vid)s, %(contribution)s)
-						'''
-	select_stmt = ''' 	SELECT *
-						FROM authors
-						WHERE pid = %(pid)s
-						 AND bid = %(bid)s
-						 AND vid = %(vid)s
-						 AND contribution = %(contribution)s
-						'''
+	
 	for key in bill['versions'].keys():
 		a = dict()
 		pid = get_pid_db(bill['first'], bill['last'], dddb)
 		if pid is not None and check_bid_db(bill['bid'], dddb):
+
 			a['pid'] = pid
 			a['bid'] = bill['bid']
 			a['vid'] = bill['bid'] + key
 			a['contribution'] = 'Lead Author'
 #			print a['vid']
-			dddb.execute(select_stmt, a)
-			if dddb.rowcount == 0 and check_vid_db(a['vid'], dddb):
-				dddb.execute(insert_stmt, a)
+			vid_check = check_vid_db(a['vid'], dddb)
+			dddb.execute(QS_AUTHORS_CHECK, a)
+			if dddb.rowcount == 0 and vid_check:
+				dddb.execute(QI_AUTHORS, a)
 				counter += 1
+			elif vid_check and dddb.fetchone()[0] != a['pid']:
+				dddb.execute(QU_AUTHORS, a)
+				print 'updated', a['pid']
 #			else:
 #				print a['bid'], "already existing"
 #		else:
 #			print "fill Person, Bill table first"
 
 def check_vid_db(vid, dddb):
-	select_stmt = '''	SELECT * FROM BillVersion
-						WHERE vid = %(vid)s
-						'''
-	dddb.execute(select_stmt, {'vid':vid})
+	dddb.execute(QS_BILLVERSION, (vid,))
 	if dddb.rowcount == 1:
 		return True
 	else:
@@ -93,10 +121,7 @@ def check_vid_db(vid, dddb):
 		return False 
 
 def check_bid_db(bid, dddb):
-	select_stmt = '''	SELECT * FROM Bill
-						WHERE bid = %(bid)s
-						'''
-	dddb.execute(select_stmt, {'bid':bid})
+	dddb.execute(QS_BILL, (bid,))
 	if dddb.rowcount == 1:
 		return True
 	else:
@@ -110,33 +135,37 @@ def clean_name(name):
         "Thomas McKevitt":("Tom", "McKevitt"), 
         "Albert Stirpe":("Al","Stirpe"), 
         "Peter Abbate":("Peter","Abbate, Jr."),
-        "Sam Roberts":("Pamela","Hunter"),
+#        "Sam Roberts":("Pamela","Hunter"),
         "Herman Farrell":("Herman", "Farrell, Jr."),
         "Fred Thiele":("Fred", "Thiele, Jr."),
-        "William Scarborough":("Alicia", "Hyndman"),
+#       "William Scarborough":("Alicia", "Hyndman"),
         "Robert Oaks":("Bob", "Oaks"),
         "Andrew Goodell":("Andy", "Goodell"),
         "Peter Rivera":("José", "Rivera"),
         "Addie Jenne Russell":("Addie","Russell"),
         "Kenneth Blankenbush":("Ken","Blankenbush"),
-        "Alec Brook-Krasny":("Pamela","Harris"),
+#        "Alec Brook-Krasny":("Pamela","Harris"),
         "Mickey Kearns":("Michael", "Kearns"),
         "Steven Englebright":("Steve", "Englebright"),
-        
+        "HUNTER":("Pamela","Hunter"),
     }
     ending = {'Jr':', Jr.','Sr':', Sr.','II':' II','III':' III', 'IV':' IV'}
     name = name.replace(',', ' ')
     name = name.replace('.', ' ')
     name = name.replace('  ', ' ')
     name_arr = name.split()      
-    suffix = "";               
+    suffix = "";
+    if len(name_arr) == 1 and name_arr[0] in problem_names.keys():
+    	print name_arr
+    	name_arr = list(problem_names[name_arr[0]])
+    	print name_arr
     for word in name_arr:
 #    	print "word", word
         if word != name_arr[0] and (len(word) <= 1 or word in ending.keys()):
             name_arr.remove(word)
             if word in ending.keys():
                 suffix = ending[word]            
-            
+#    print name_arr        
     first = name_arr.pop(0)
 #    print "first", first
     while len(name_arr) > 1:
@@ -151,13 +180,8 @@ def clean_name(name):
     return (first, last)
 
 def get_pid_db(first, last, dddb):
-	select_person = '''	SELECT * FROM Person
-                     	WHERE first = %(first)s
-                      	 AND last = %(last)s 
-                  		'''
-	dddb.execute(select_person, {'first':first,'last':last})
-#	print (select_person %  {'first':first,'last':last})
-	if dddb.rowcount == 1:
+	dddb.execute(QS_PERSON, (last, first))
+	if dddb.rowcount >= 1:
 		ret = dddb.fetchone()[0]
 		return ret
 	else:
@@ -171,14 +195,14 @@ def add_authors_db(year, dddb):
 		insert_authors_db(bill, dddb)
 
 def main():
-	dddb_conn =  MySQLdb.connect(host='digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com',
+	with loggingdb.connect(host='digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com',
 						user='awsDB',
-						db='JohnTest',
+						db='DDDB2015Dec',
 						port=3306,
 						passwd='digitaldemocracy789',
-						charset='utf8')
-	dddb = dddb_conn.cursor()
-	dddb_conn.autocommit(True)
-	add_authors_db(2015, dddb)
+						charset='utf8') as dddb:
+#		dddb = dddb_conn.cursor()
+#		dddb_conn.autocommit(True)
+		add_authors_db(2015, dddb)
 	print counter
 main()
