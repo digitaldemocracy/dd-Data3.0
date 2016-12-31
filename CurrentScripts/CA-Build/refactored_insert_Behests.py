fe8d046b6ee72287862ef3f382366858e7f28bbf
parose <paro42796@gmail.com>
2016-12-31 02:57:01 +0000

    insert_behests now adds sessionYear and downloads new data

--- CurrentScripts/CA-Build/refactored_insert_Behests.py
+++ CurrentScripts/CA-Build/refactored_insert_Behests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python
 '''
 File: insert_Behests.py
 Author: Mandy Chan
@@ -21,8 +21,8 @@ Usage:
 
 Source:
   - California Fair Political Practices Commission (fppc.ca.gov/index.php?id=499)
-    - YYYY-Senate.csv
-    - YYYY-Assembly.csv
+    - YYYY-Senate.xlsx
+    - YYYY-Assembly.xlsx
 
 Populates:
   - Behests (official, datePaid, payor, amount, payee, description, purpose, noticeReceived)
@@ -35,20 +35,26 @@ import os
 import re
 import sys
 import csv
-import loggingdb
+import requests
 import MySQLdb
+import openpyxl
 from pprint import pprint
 from datetime import datetime
 
 # Global Data
+#URLS for behest data
+ASSEMBLY_URL = "http://www.fppc.ca.gov/content/dam/fppc/documents/behested-payments/2016/2016_Assembly.xlsx"
+
+SENATE_URL = "http://www.fppc.ca.gov/content/dam/fppc/documents/behested-payments/2016/2016_Senate.xlsx"
+
 # Number of cmdline arguments needed
 NUM_ARGS = 2
 
 # Column names dictionary
 COL = {
-  'official':0, 'pay_date':1, 'payor':2,       'payor_city':3, 'payor_state':4,
-  'amount':5,   'payee':6,    'payee_city':7,  'payee_state':8,
-  'descr':9,    'purpose':10, 'notice_rec':11, 'ytd':12
+  'official': 0, 'pay_date': 1, 'payor': 2, 'payor_city': 3, 'payor_state': 4,
+  'amount': 5,   'payee': 6,    'payee_city': 7,  'payee_state': 8,
+  'descr': 9,    'purpose': 10, 'notice_rec': 11, 'ytd': 12
 }
 
 # Dictionary of problematic legislator names. Add as necessary.
@@ -100,7 +106,7 @@ Creates a row in Organizations table.
 '''
 def create_organization(dd_cursor, org, city, state):
   insert_stmt = '''INSERT INTO Organizations
-                   (name, city, state)
+                   (name, city, stateHeadquartered)
                    VALUES
                    (%(name)s, %(city)s, %(state)s);
                 '''
@@ -117,7 +123,7 @@ def find_organization(dd_cursor, org, city, state):
                    FROM Organizations
                    WHERE name = %(name)s
                     AND city = %(city)s
-                    AND state = %(state)s
+                    AND stateHeadquartered = %(state)s
                 '''
   dd_cursor.execute(select_stmt, {'name':org, 'city':city, 'state':state})
   query = dd_cursor.fetchone()
@@ -157,22 +163,40 @@ Check if there is already a behest with the same exact information
 '''
 def duplicate_behest(dd_cursor,
     off_pid, date_paid, payor_id, amt, payee_id, descr, purpose, notice_rec):
+  #select_stmt = '''SELECT *
+  #                 FROM Behests
+  #                 WHERE official = %(off_pid)s
+  #                  AND datePaid = %(date)s AND payor = %(payor)s
+  #                  AND amount = %(amt)s AND payee = %(payee)s
+  #                  AND description = %(descr)s AND purpose = %(purpose)s
+  #                  AND noticeReceived = %(notice_rec)s
+  #              '''
+  #dd_cursor.execute(select_stmt, {'off_pid':off_pid, 'date':date_paid, 
+  #  'payor':payor_id, 'amt':amt, 'payee':payee_id, 'descr':descr,
+  #  'purpose':purpose, 'notice_rec':notice_rec})
+
   select_stmt = '''SELECT *
                    FROM Behests
                    WHERE official = %(off_pid)s
                     AND datePaid = %(date)s AND payor = %(payor)s
-                    AND amount = %(amt)s AND payee = %(payee)s
-                    AND description = %(descr)s AND purpose = %(purpose)s
-                    AND noticeReceived = %(notice_rec)s
-                '''
-  dd_cursor.execute(select_stmt, {'off_pid':off_pid, 'date':date_paid, 
-    'payor':payor_id, 'amt':amt, 'payee':payee_id, 'descr':descr,
-    'purpose':purpose, 'notice_rec':notice_rec})
+                    AND payee = %(payee)s'''
+
+  dd_cursor.execute(select_stmt, {'off_pid':off_pid, 'date':date_paid,
+    'payor':payor_id, 'payee':payee_id})
 
   query = dd_cursor.fetchone()
   if query is None:
     return False
   return True
+
+def get_session_year(dd_cursor):
+    select_stmt = '''SELECT MAX(start_year)
+                     FROM Session
+                     WHERE state = 'CA'
+                  '''
+    dd_cursor.execute(select_stmt)
+    year = dd_cursor.fetchone()[0]
+    return year
 
 '''
 Insert row into Behests
@@ -183,14 +207,21 @@ def create_behest(dd_cursor,
       descr, purpose, notice_rec):
     return -1
 
+  session_year = get_session_year(dd_cursor)
+
   insert_stmt = '''INSERT INTO Behests
+                   (official, datePaid, payor, amount, payee, description, purpose, noticeReceived, sessionYear)
                    VALUES
                    (%(off_pid)s, %(datePaid)s, %(payor_id)s, %(amount)s, 
-                   %(payee_id)s, %(descr)s, %(purpose)s, %(notice_rec)s)
-                '''  
-  dd_cursor.execute(insert_stmt, {'off_pid':off_pid, 'datePaid':date_paid,
+                     %(payee_id)s, %(descr)s, %(purpose)s, %(notice_rec)s, %(session_year)s)
+                ''' 
+  
+  #dd_cursor.execute(insert_stmt, 
+  behest_dict = {'off_pid':off_pid, 'datePaid':date_paid,
     'payor_id':payor_id, 'amount':amt, 'payee_id':payee_id, 'descr':descr,
-    'purpose':purpose, 'notice_rec':notice_rec})
+    'purpose':purpose, 'notice_rec':notice_rec, 'session_year':session_year}
+  print(behest_dict)
+  dd_cursor.execute(insert_stmt, behest_dict)
 
 '''
 Parse the row and insert the information to DDDB. It then returns the current 
@@ -199,11 +230,11 @@ line. (see Behest data)
 '''
 def parse_row(dd_cursor, attribs, official):
   # Assume if Official and Payor are blank, the line is unnecessary
-  if attribs[COL['official']] == '' and attribs[COL['payor']] == '':
+  if attribs[COL['official']].value == None and attribs[COL['payor']].value == None:
     return official
   # If legislator/official name is present in this row, find their pid
-  elif attribs[COL['official']] != '':
-    official = find_official(dd_cursor, attribs[COL['official']].strip())
+  elif attribs[COL['official']].value != None:
+    official = find_official(dd_cursor, attribs[COL['official']].value.strip())
   
   name = official[0]
   pid = official[1]
@@ -215,36 +246,39 @@ def parse_row(dd_cursor, attribs, official):
 
   # Find Payor id from Payors table
   payor_id = find_payor(dd_cursor,
-      attribs[COL['payor']].strip(), attribs[COL['payor_city']].strip(),
-      attribs[COL['payor_state']].strip())
+      attribs[COL['payor']].value.strip(), attribs[COL['payor_city']].value.strip(),
+      attribs[COL['payor_state']].value.strip())
 
   # Find Payee id from Organizations table 
   payee_id = find_organization(dd_cursor,
-      attribs[COL['payee']].strip(), attribs[COL['payee_city']].strip(),
-      attribs[COL['payee_state']].strip())
+      attribs[COL['payee']].value.strip(), attribs[COL['payee_city']].value.strip(),
+      attribs[COL['payee_state']].value.strip())
 
   # Rest of variables
-  date_paid = attribs[COL['pay_date']]
-  amount = attribs[COL['amount']].replace(',', '')
-  descr = attribs[COL['descr']]
-  purpose = attribs[COL['purpose']]
-  notice_rec = None if attribs[COL['notice_rec']] == 'No date' else (
-      datetime.strptime(attribs[COL['notice_rec']], '%m/%d/%y').
-      strftime('%Y-%m-%d'))
+  date_paid = attribs[COL['pay_date']].value
+  amount = attribs[COL['amount']].value
+  descr = attribs[COL['descr']].value
+  purpose = attribs[COL['purpose']].value
+  notice_rec = None if attribs[COL['notice_rec']].value == 'No date' or type(attribs[COL['notice_rec']].value) is not datetime else (
+      attribs[COL['notice_rec']].value)
 
   # Format dates correctly
   try:
-    date_paid = (datetime.strptime(date_paid, '%m/%d/%y').
-        strftime('%Y-%m-%d'))
-  except ValueError:
-    pattern = '\d{1,2}/\d{1,2}-\d{1,2}/\d{1,2}/\d{2}'
-    if re.match(pattern, date_paid):
-      date_paid = date_paid.split('-')[1]
+      date_paid = date_paid.strftime('%Y-%m-%d')
+  except AttributeError:
+    #pattern = '\d{1,2}/\d{1,2}-\d{1,2}/\d{1,2}/\d{2}'
+    #if re.match(pattern, date_paid):
+    date_paid = date_paid.split('-')[0].strip()
+    print(date_paid)
+    try:
       date_paid = (datetime.strptime(date_paid, '%m/%d/%y').
-        strftime('%Y-%m-%d'))
-    else:
-      print('Incorrect date format! %s' % date_paid)
-
+       strftime('%Y-%m-%d'))
+    except ValueError:
+      date_paid = (datetime.strptime(date_paid, '%m/%Y').
+       strftime('%Y-%m-%d'))
+    #else:
+    #  print('Incorrect date format! %s' % date_paid)
+ 
   create_behest(dd_cursor, pid, date_paid, payor_id, amount, payee_id, descr, 
       purpose, notice_rec)
   return official
@@ -264,31 +298,55 @@ def check_args(args):
     print('Please use .csv file')
     raise Exception('Bad arguments')
 
+def download_behests():
+  with open("senate_behests.xlsx", "wb") as file:
+    response = requests.get(SENATE_URL)
+    file.write(response.content)
+    file.close
+
+  with open("assembly_behests.xlsx", "wb") as file:
+    response = requests.get(ASSEMBLY_URL)
+    file.write(response.content)
+    file.close
+
 def main():
   # Current Official (as (name, pid) tuple)
-  cur_official = ('', -1)
+  cur_official = (None, -1)
 
   # Check arguments
-  check_args(sys.argv)
-  file_name = sys.argv[1]
+  #check_args(sys.argv)
+  #file_name = sys.argv[1]
+
+  download_behests()
 
   # Opening db connection
-  with loggingdb.connect(host='digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com',
-                       port=3306,
-                       db='DDDB2015July',
-                       user='awsDB',
-                       passwd='digitaldemocracy789') as dd_cursor:
+  #with MySQLdb.connect(host='digitaldemocracydb.chzg5zpujwmo.us-west-2.rds.amazonaws.com',
+  #                     port=3306,
+  #                     db='DDDB2015July',
+  #                     user='awsDB',
+  #                     passwd='digitaldemocracy789') as dd_cursor:
 
+  with MySQLdb.connect(host = 'dev.digitaldemocracy.org',
+                       port = 3306,
+                       db = 'parose_dddb',
+                       user = 'parose',
+                       passwd = 'parose221') as dd_cursor:
     # Opening file and start at the header_line number
-    with open(file_name, 'r') as f:
-      reader = csv.reader(f, skipinitialspace=True)
-      for row in reader:
+    assembly = openpyxl.load_workbook("assembly_behests.xlsx", data_only = True)
+    assembly_ws = assembly['Sheet1']
 
-        # TESTING PURPOSES
-        #print('%(line)s: %(attributes)s' % {'line':reader.line_num, 'attributes':row})
-        #print('%(official)s' % {'official': cur_official})
+    for row in assembly_ws.iter_rows(min_row = 6, max_row = assembly_ws.max_row):
+    #if row[COL['official']].value != None:
+      #print row[COL['official']].value
+      cur_official = parse_row(dd_cursor, row, cur_official)
 
-        cur_official = parse_row(dd_cursor, row, cur_official)
+    senate = openpyxl.load_workbook("senate_behests.xlsx", data_only = True)
+    senate_ws = senate['Sheet1']
+
+    for row in senate_ws.iter_rows(min_row = 6, max_row = senate_ws.max_row):
+      #if row[COL['official']].value != None:
+        #print row[COL['official']].value
+      cur_official = parse_row(dd_cursor, row, cur_official)
 
 if __name__ == "__main__":
   main()
