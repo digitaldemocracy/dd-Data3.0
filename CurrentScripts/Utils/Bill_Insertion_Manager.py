14f220222425722d423225733a0a7ccfe71a22bb
parose <paro42796@gmail.com>
2017-07-11 21:40:09 +0000

    Adds docstrings to Bill_Insertion_Manager

--- CurrentScripts/Utils/Bill_Insertion_Manager.py
+++ CurrentScripts/Utils/Bill_Insertion_Manager.py
@@ -42,10 +42,11 @@ class BillInsertionManager(object):
         self.logger = logger
         self.state = state
 
-    '''
-    Handles DW and GrayLogger logging
-    '''
+
     def log(self):
+        """
+        Handles logging. Should be called immediately before the insertion script finishes.
+        """
         LOG = {'tables': [{'state': self.state, 'name': 'Bill', 'inserted': self.B_INSERTED, 'updated': 0, 'deleted': 0},
                           {'state': self.state, 'name': 'Motion', 'inserted': self.M_INSERTED, 'updated': 0, 'deleted': 0},
                           {'state': self.state, 'name': 'BillVoteSummary', 'inserted': self.BVS_INSERTED, 'updated': 0, 'deleted': 0},
@@ -56,35 +57,40 @@ class BillInsertionManager(object):
         sys.stderr.write(json.dumps(LOG))
 
 
-    '''
-    Checks if a bill already exists in the database.
-    '''
     def is_bill_in_db(self, bill):
+        """
+        Checks if a bill exists in the database
+        :param bill: A dictionary returned by a Bill object's to_dict method
+        :return: True if the bill is in the database, false otherwise
+        """
         return is_entity_in_db(self.dddb, SELECT_BILL, bill, 'Bill', self.logger)
 
-    '''
-    Inserts a bill into the database.
-    Returns true if the insert succeeds, false otherwise.
-    '''
     def insert_bill(self, bill):
+        """
+        Inserts a bill into the database
+        :param bill: A dictionary returned by a Bill object's to_dict method
+        :return: True if the insert succeeds, false otherwise
+        """
         return insert_entity(db_cursor=self.dddb,
                              entity=bill,
                              qi_query=INSERT_BILL,
                              objType="Bill",
                              logger=self.logger)
 
-    '''
-    Checks if a motion exists in the database.
-    Returns the motion's MID if it exists, None otherwise.
-    '''
     def get_motion_id(self, motion):
+        """
+        Checks if a motion exists in the database
+        :param motion: A dictionary containing information on a motion
+        :return: True if the motion is in the database, false otherwise
+        """
         return get_entity_id(self.dddb, SELECT_MOTION, motion, 'Motion', self.logger)
 
-    '''
-    Inserts a new motion into the database.
-    Returns true if the insert succeeds, false otherwise.
-    '''
     def insert_motion(self, motion):
+        """
+        Inserts a motion into the database
+        :param motion: A dictionary containing information on a motion
+        :return: True if the insert succeeds, false otherwise
+        """
         self.dddb.execute(SELECT_LAST_MID)
         mid = self.dddb.fetchone()[0]
         mid += 1
@@ -97,52 +103,60 @@ class BillInsertionManager(object):
                              objType="Motion",
                              logger=self.logger)
 
-    '''
-    Gets the VoteId of a BillVoteSummary in the database.
-    Returns false if the BillVoteSummary does not exist.
-    '''
     def get_vote_id(self, vote):
+        """
+        Gets the VoteId of a BillVoteSummary in the database
+        :param vote: A dictionary returned by a Vote object's to_dict method
+        :return: The VoteId of the vote, or false if no VoteId is found
+        """
         return get_entity_id(self.dddb, SELECT_VOTE, vote, 'BillVoteSummary', self.logger)
 
-    '''
-    Inserts a BillVoteSummary into the database.
-    Returns true if the insert succeeds, false otherwise.
-    '''
-    def insert_bvs(self, vote):
+    def insert_bill_vote_summary(self, vote):
+        """
+        Inserts a BillVoteSummary into the database
+        :param vote: A dictionary returned by a Vote object's to_dict method
+        :return: True if the insert succeeds, false otherwise
+        """
         return insert_entity(db_cursor=self.dddb,
                              entity=vote,
                              qi_query=INSERT_BVS,
                              objType="BillVoteSummary",
                              logger=self.logger)
 
-    '''
-    Checks if a BillVoteDetail exists in the database.
-    '''
-    def is_bvd_in_db(self, bvd):
+    def is_bill_vote_detail_in_db(self, bvd):
+        """
+        Checks if a BillVoteDetail exists in the database
+        :param bvd: A dictionary returned by a VoteDetail object's to_dict method
+        :return: True if the vote detail is in the database, false otherwise
+        """
         return is_entity_in_db(self.dddb, SELECT_BVD, bvd, 'BillVoteDetail', self.logger)
 
-    '''
-    Inserts a BillVoteDetail into the database.
-    Returns true if the insert succeeds, false otherwise.
-    '''
-    def insert_bvd(self, bvd):
+    def insert_bill_vote_detail(self, bvd):
+        """
+        Inserts a BillVoteDetail into the database
+        :param bvd: A dictionary returned by a VoteDetail object's to_dict method
+        :return: True if the vote detail is in the database, false otherwise
+        """
         return insert_entity(db_cursor=self.dddb,
                              entity=bvd,
                              qi_query=INSERT_BVD,
                              objType="BillVoteDetail",
                              logger=self.logger)
 
-    '''
-    Checks if a BillVersion exists in the database.
-    '''
     def is_version_in_db(self, version):
+        """
+        Checks if a BillVersion exists in the database
+        :param version: A dictionary returned by a Version object's to_dict method
+        :return: True if the version is in the database, false otherwise
+        """
         return is_entity_in_db(self.dddb, SELECT_VERSION, version, 'BillVersion', self.logger)
 
-    '''
-    Inserts a BillVersion into the database.
-    Returns true if the insert succeeds, false otherwise.
-    '''
     def insert_version(self, version):
+        """
+        Inserts a BillVersion into the database
+        :param version: A dictionary returned by a Version object's to_dict method
+        :return: True if the vote detail is in the database, false otherwise
+        """
         return insert_entity(db_cursor=self.dddb,
                              entity=version,
                              qi_query=INSERT_VERSION,
@@ -152,7 +166,7 @@ class BillInsertionManager(object):
     def check_version_text(self, version):
         """
         Checks if the version text is null
-        :param version: A dictionary returned from a Version object's to_dict() method
+        :param version: A dictionary returned from a Version object's to_dict method
         :return: True if the version text is not null, False otherwise
         """
         try:
@@ -172,33 +186,37 @@ class BillInsertionManager(object):
     def update_version_text(self, version):
         """
         Updates the text column in the BillVersion table
-        :param version: A dictionary returned from a Version object's to_dict() method
+        :param version: A dictionary returned from a Version object's to_dict method
         :return: True if the update succeeds, false otherwise
         """
         return update_entity(self.dddb, UPDATE_VERSION_TEXT, version, "BillVersion", self.logger)
 
-
-    '''
-    Checks if an Action exists in the database.
-    '''
     def is_action_in_db(self, action):
+        """
+        Checks if an Action exists in the database
+        :param action: A dictionary returned from an Action object's to_dict method
+        :return: True if the action is in the database, false otherwise
+        """
         return is_entity_in_db(self.dddb, SELECT_ACTION, action, 'BillVersion', self.logger)
 
-    '''
-    Inserts an Action into the database.
-    Returns true if the insert succeeds, false otherwise.
-    '''
     def insert_action(self, action):
+        """
+        Inserts an action into the database
+        :param action: A dictionary returned from an Action object's to_dict method
+        :return: True if the insert succeeds, false otherwise
+        """
         return insert_entity(db_cursor=self.dddb,
                              entity=action,
                              qi_query=INSERT_ACTION,
                              objType="BillVoteDetail",
                              logger=self.logger)
 
-    '''
-    This function formats and adds a bill to the database if it doesn't exist.
-    '''
     def add_bills_db(self, bill_list):
+        """
+        This function handles inserting bills and their related information into the database
+        :param bill_list: A list of bills to insert into the database
+        :return: True if all the inserts succeed, false otherwise
+        """
         for bill in bill_list:
             if not self.is_bill_in_db(bill.to_dict()):
                 if not self.insert_bill(bill.to_dict()):
@@ -215,10 +233,12 @@ class BillInsertionManager(object):
 
         return True
 
-    '''
-    This function formats and adds a bill's votes to the database.
-    '''
     def add_votes_db(self, vote_list):
+        """
+        This function handles adding information on a bill's votes to the database
+        :param vote_list: A list of votes made on a certain bill
+        :return: True if all the inserts succeed, false otherwise
+        """
         for vote in vote_list:
             # Get motion ID or insert new motio
             mid = self.get_motion_id(vote.motion_dict())
@@ -249,10 +269,12 @@ class BillInsertionManager(object):
 
         return True
 
-    '''
-    This function formats and adds a bill's versions to the database.
-    '''
     def add_versions_db(self, version_list):
+        """
+        This function handles adding information on a bill's versions to the database
+        :param version_list: A list of a bill's versions
+        :return: True if all the inserts succeed, false otherwise
+        """
         for version in version_list:
             if not self.is_version_in_db(version.to_dict()):
                 if not self.insert_version(version.to_dict()):
@@ -264,10 +286,12 @@ class BillInsertionManager(object):
 
         return True
 
-    '''
-    This function formats and adds a bill's actions to the database.
-    '''
     def add_actions_db(self, action_list):
+        """
+        This function handles adding information on a bill's actions to the database
+        :param action_list: A list of a bill's actions
+        :return: True if all the inserts succeed, false otherwise
+        """
         for action in action_list:
             if not self.is_action_in_db(action.to_dict()):
                 if not self.insert_action(action.to_dict()):
