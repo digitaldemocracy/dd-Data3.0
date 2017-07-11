99cf65d974c68ae54a36429155e99f8b0b884f7e
Nicholas Russo <nrusso19@gmail.com>
2017-07-10 19:36:16 -0700

    Adding CA run command

---
+++ CurrentScripts/ca_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./CA-Build/ca_import_action.py
+./CA-Build/ca_import_author.py
+./CA-Build/ca_import_bills.py
+./CA-Build/ca_bill_parse.py
+./CA-Build/ca_import_lobbyist.py
+./CA-Build/ca_import_web_committees.py
+./CA-Build/ca_import_motion.py
+./CA-Build/ca_import_vote.py
+./CA-Build/TSV_extract.py
