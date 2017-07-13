dee494020a69bbb53bffcc1d0fced29cb4219cf7
root <root@ip-172-31-37-21.us-west-2.compute.internal>
2017-07-13 00:12:42 +0000

    DW version of run ca

--- CurrentScripts/run/run_ca.sh
+++ CurrentScripts/run/run_ca.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-./home/narusso/master/CurrentScripts/CA-Build/ca_import_bills.py &&
-./home/narusso/master/CurrentScripts/CA-Build/ca_bill_parse.py &&
-./home/narusso/master/CurrentScripts/CA-Build/ca_import_action.py &&
-./home/narusso/master/CurrentScripts/CA-Build/ca_import_author.py &&
-./home/narusso/master/CurrentScripts/CA-Build/ca_import_lobbyist.py &&
-./home/narusso/master/CurrentScripts/CA-Build/ca_import_web_committees.py &&
-./home/narusso/master/CurrentScripts/CA-Build/ca_import_motion.py &&
-./home/narusso/master/CurrentScripts/CA-Build/ca_import_vote.py &&
-./home/narusso/master/CurrentScripts/CA-Build/TSV_extract.py
+export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
+
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_bills.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_bill_parse.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_action.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_authors.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_web_committees.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_motion.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_vote.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/TSV_extract.py &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_lobbyists.py
