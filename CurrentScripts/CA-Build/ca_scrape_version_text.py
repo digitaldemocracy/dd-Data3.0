1beae952708782a0d79dbba51e3949e29ac7eabf
parose <paro42796@gmail.com>
2017-07-19 17:34:20 +0000

    Renamed ca_bill_parse to ca_scrape_version_text

--- CurrentScripts/CA-Build/ca_scrape_version_text.py
+++ CurrentScripts/CA-Build/ca_scrape_version_text.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-'''
+
+"""
 File: billparse.py
 Author: ???
-Modified By: Daniel Mangin, Mandy Chan, Steven Thon, Freddy Hernandez
+Modified By: Daniel Mangin, Mandy Chan, Steven Thon, Freddy Hernandez, Andrew Rose
 Date: 6/11/2015
 
 Description:
@@ -26,13 +27,22 @@ Sources:
 
 Populates:
   - BillVersion (title, digest, text, state)
-'''
+"""
+
+import sys
 import json
 import unicodedata
 import datetime as dt
 from lxml import etree
+from Models.Version import *
+from bs4 import BeautifulSoup
 from Utils.Generic_Utils import *
 from Utils.Database_Connection import *
+from Utils.Bill_Insertion_Manager import *
+from Constants.Bills_Queries import *
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 logger = None
 UPDATE = 0
@@ -41,18 +51,22 @@ UPDATE = 0
 STATE = 'CA'
 
 # Queries
-QS_CPUB_BILL_VERSION = '''SELECT bill_version_id, bill_xml
-                          FROM bill_version_tbl
-                          WHERE trans_update > %(updated_since)s
-                          '''
+# QS_CPUB_BILL_VERSION = '''SELECT bill_version_id, bill_xml
+#                           FROM bill_version_tbl
+#                           WHERE trans_update > %(updated_since)s
+#                           '''
 QU_BILL_VERSION = '''UPDATE BillVersion
                      SET title = %s, digest= %s, text = %s, state = %s
                      WHERE vid = %s'''
 
 
-'''Tries to remove unmatched html tags from an xml string.
-'''
 def remove_unmatched_tags(xml):
+    """
+    Removes unmatched HTML tags from an XML string
+    :param xml: The XML string to parse
+    :return: The XML string with unmatched tags removed
+    """
+
     # The following regex courtesy of:
     # http://haacked.com/archive/2004/10/25/usingregularexpressionstomatchhtml.aspx/
     pat_str = r'''
@@ -87,18 +101,29 @@ def remove_unmatched_tags(xml):
         chars_deleted += len(m.group())
     return xml
 
+
 '''The xml files from capublic have several issues. This function employs
 several methods to attempt to fix them.
 '''
 def sanitize_xml(xml):
+    """
+    The XML files from CAPublic have several issues. This function
+    fixes these issues.
+    :param xml: The XML file from CAPublic
+    :return: The sanitized XML string
+    """
+
     # Bad strings present in the xml, along with their corresponding
     # replacements.
     replacement_patterns = [
-        (r'&lt;xhtml:', '<'),   # Specific case 1.
-        (r'&lt;/xhtml:', '</'), # Specific case 2.
+        (r'&lt;', '<'),   # Specific case 1.
+        (r'&lt;', '</'), # Specific case 2.
         (r'&quot;', '"'),
         (r'<<', '<')
     ]
+
+    for pat, repl in replacement_patterns:
+        xml = re.sub(pat, repl, xml)
 
     xml = xml.strip()
     xml = re.sub(r'<\?xm-(insertion|deletion)_mark\?>', r'', xml)
@@ -115,16 +140,18 @@ def sanitize_xml(xml):
     xml = remove_unmatched_tags(xml)
     return xml
 
+
 def get_bill_versions(ca_cursor):
     updated_date = dt.date.today() - dt.timedelta(weeks=1)
     updated_date = updated_date.strftime('%Y-%m-%d')
 
-    ca_cursor.execute(QS_CPUB_BILL_VERSION, {'updated_since': updated_date})
+    ca_cursor.execute(SELECT_CAPUBLIC_VERSION_XML, {'updated_since': updated_date})
 
     for vid, xml in ca_cursor.fetchall():
         if xml is None:
             continue
         yield '%s_%s' % (STATE, vid), sanitize_xml(xml)
+
 
 def billparse(ca_cursor, dd_cursor):
     global UPDATE
@@ -149,26 +176,50 @@ def billparse(ca_cursor, dd_cursor):
             # If there isn't a caml:Bill tag, then there must
             # be a caml:Content tag.
             body = extract_caml('Content')
-        try:
-            dd_cursor.execute(QU_BILL_VERSION, (title, digest, body, STATE, vid))
-            UPDATE += dd_cursor.rowcount
-        except MySQLdb.Error:
-            logger.exception(format_logger_message('Insert Failed for BillVersion',
-                                                            (QU_BILL_VERSION % (title, digest, body, STATE, vid))))
+
+        #print(title)
+        #print(digest)
+
+        with open('formatted_test.xml', 'w') as f:
+            f.write(body)
+
+        # try:
+        #     dd_cursor.execute(QU_BILL_VERSION, (title, digest, body, STATE, vid))
+        #     UPDATE += dd_cursor.rowcount
+        # except MySQLdb.Error:
+        #     logger.exception(format_logger_message('Insert Failed for BillVersion',
+        #                                                     (QU_BILL_VERSION % (title, digest, body, STATE, vid))))
+
+
+def main():
+    with connect() as dd_cursor:
+        with connect_to_capublic() as ca_cursor:
+            bill_manager = BillInsertionManager(dd_cursor, logger, 'CA')
+
+            billparse(ca_cursor, dd_cursor)
+
+            #bill_manager.log()
+
+            #with open("test.xml", 'w') as xmlfile:
+            #xmlfile.write(ca_cursor.fetchone()[1])
+
 
 if __name__ == "__main__":
-    with connect() as dd_cursor:
-        with MySQLdb.connect(host='transcription.digitaldemocracy.org',
-                             user='monty',
-                             db='capublic',
-                             passwd='python',
-                             #host='localhost',
-                             #user='root',
-                             #db='historic_capublic',
-                             #passwd='',
-                             charset='utf8') as ca_cursor:
-            logger = create_logger()
-            billparse(ca_cursor, dd_cursor)
-    LOG = {'tables': [{'state': 'CA', 'name': 'BillVersion', 'inserted':0 , 'updated': UPDATE, 'deleted': 0}]}
-    sys.stderr.write(json.dumps(LOG))
-    logger.info(LOG)
+    logger = create_logger()
+    main()
+
+    # with connect() as dd_cursor:
+    #     with MySQLdb.connect(host='transcription.digitaldemocracy.org',
+    #                          user='monty',
+    #                          db='capublic',
+    #                          passwd='python',
+    #                          #host='localhost',
+    #                          #user='root',
+    #                          #db='historic_capublic',
+    #                          #passwd='',
+    #                          charset='utf8') as ca_cursor:
+    #         logger = create_logger()
+    #         billparse(ca_cursor, dd_cursor)
+    # LOG = {'tables': [{'state': 'CA', 'name': 'BillVersion', 'inserted':0 , 'updated': UPDATE, 'deleted': 0}]}
+    # sys.stderr.write(json.dumps(LOG))
+    # logger.info(LOG)
