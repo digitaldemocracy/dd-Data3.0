243916db6c8902ac7753a6850e5c9e15f9fb1739
Nicholas Russo <nrusso19@gmail.com>
2017-08-09 11:03:55 -0700

    Removed bill_parse file

--- CurrentScripts/run/run_ca.sh
+++ CurrentScripts/run/run_ca.sh
@@ -4,7 +4,6 @@ export SCRIPTPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_bills.py -q &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_bill_parse.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_action.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_authors.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_committees.py -q &&
