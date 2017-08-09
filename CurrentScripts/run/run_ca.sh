28f41be36bb1a1b9d8a69c1d7065e92da4fe9540
Nicholas Russo <nrusso19@gmail.com>
2017-08-09 10:06:07 -0700

    Updated path

--- CurrentScripts/run/run_ca.sh
+++ CurrentScripts/run/run_ca.sh
@@ -3,12 +3,12 @@
 export SCRIPTPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_bills.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_bill_parse.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_action.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_authors.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_committees.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_motion.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_vote.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/TSV_extract.py -q
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_lobbyists.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_bills.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_bill_parse.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_action.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_authors.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_committees.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_motion.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_vote.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/TSV_extract.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_lobbyists.py -q
