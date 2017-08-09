4dc3aedb576007aa5e2b5af1158a4efb12142a47
Nicholas Russo <nrusso19@gmail.com>
2017-08-08 19:02:23 -0700

    Added -q to scripts to prevent emails. Trying to remove logger printing to console.

--- CurrentScripts/run/run_ca.sh
+++ CurrentScripts/run/run_ca.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+export SCRIPTPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_bills.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_bill_parse.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_action.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_authors.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_web_committees.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_motion.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_vote.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/TSV_extract.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_lobbyists.py
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_bills.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_bill_parse.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_action.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_authors.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_committees.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_motion.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_vote.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/TSV_extract.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA-Build/ca_import_lobbyists.py -q
--- CurrentScripts/run/run_fl.sh
+++ CurrentScripts/run/run_fl.sh
@@ -3,7 +3,7 @@
 export SCRIPTPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_legislators.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_committees.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_bills.py &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_hearings.py
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_legislators.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_committees.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_bills.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_hearings.py -q
