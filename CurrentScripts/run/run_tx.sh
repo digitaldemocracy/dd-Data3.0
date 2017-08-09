2ef12928323d2396771900e574ea404ec758bdd8
Nicholas Russo <nrusso19@gmail.com>
2017-08-09 10:48:15 -0700

    Typo

--- CurrentScripts/run/run_tx.sh
+++ CurrentScripts/run/run_tx.sh
@@ -6,4 +6,4 @@ export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/TX-Build/tx_import_legislators.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/TX-Build/tx_import_committees.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/TX-Build/tx_import_bills.py -q &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/TX-Build/tx_import_hearings.py -q &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/TX-Build/tx_import_hearings.py -q
