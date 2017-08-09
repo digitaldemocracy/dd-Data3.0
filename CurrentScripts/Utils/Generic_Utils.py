d6edae517eb90e2c19a5ab2e0bc8af29aed085a9
Nicholas Russo <nrusso19@gmail.com>
2017-08-08 19:04:26 -0700

    Adjusting logger level.

--- CurrentScripts/Utils/Generic_Utils.py
+++ CurrentScripts/Utils/Generic_Utils.py
@@ -77,7 +77,7 @@ def create_logger():
 
     if len(sys.argv) == 1:
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
+        console_handler.setLevel(logging.warning)
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
