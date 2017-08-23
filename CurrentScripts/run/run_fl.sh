1322cf68a8d93a9174cfd1b1e4b29b47ca9315be
Nicholas Russo <nrusso19@gmail.com>
2017-08-23 14:42:20 -0700

    Moved fl to package

--- CurrentScripts/run/run_fl.sh
+++ CurrentScripts/run/run_fl.sh
@@ -3,8 +3,8 @@
 export SCRIPTPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_legislators.py -q &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_committees.py -q &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_bills.py -q &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_hearings.py -q &&
-/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL-Build/fl_import_contributions.py -q
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL/fl_import_legislators.py -q &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL/fl_import_committees.py -q &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL/fl_import_bills.py -q &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL/fl_import_hearings.py -q &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/FL/fl_import_contributions.py -q
