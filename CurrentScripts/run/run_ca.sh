9d4bc67ece4747c51082c45555cfee7daa45cae8
Nicholas Russo <nrusso19@gmail.com>
2018-01-22 10:31:13 -0800

    added ca agendas to run script

--- CurrentScripts/run/run_ca.sh
+++ CurrentScripts/run/run_ca.sh
@@ -10,5 +10,6 @@ export PYTHONPATH=/home/data_warehouse_common/dd-Data3.0/CurrentScripts/
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_committees.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_motion.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_vote.py -q &&
+/home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_agendas.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/TSV_extract.py -q &&
 /home/data_warehouse_common/dd-Data3.0/CurrentScripts/CA/ca_import_lobbyists.py -q
