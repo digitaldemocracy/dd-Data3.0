0d4c0b7842a27a4b9b96df5b142f4ff5a7bcc9d0
Nicholas Russo <nrusso19@gmail.com>
2018-02-06 23:16:38 -0800

    final ny agendas committ?

--- CurrentScripts/NY-Build/ny_import_agendas.py
+++ CurrentScripts/NY-Build/ny_import_agendas.py
@@ -13,50 +13,6 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def main():
-<<<<<<< Updated upstream
-    with connect() as dddb:
-
-        #set all hearing agendas prior to today to inactive
-        update_hearing_agenda(dddb)
-
-        #get current session year
-        year = get_session_year(dddb)
-
-        #scrape assembly committee agendas
-        assembly_comm_hearings = get_assembly_comm_hearings()
-        for hearing in assembly_comm_hearings:
-            cid = get_cid(dddb, 'Assembly', hearing['name'], year)
-            if cid is not None:
-                hid = insert_hearing(dddb, hearing['date'], year, cid)
-            
-                if hid is not None and cid is not None:
-                    insert_comm_hearing(dddb, cid, hid)
-                bills = scrape_hearing_agenda(dddb, hearing['url'], 'Assembly')
-                if hid is not None and len(bills) > 0:
-                    for bid in bills:
-                        insert_hearing_agenda(dddb, hid, bid)
-
-        #scrape senate committee agendas
-        # senate_comm_hearings = get_senate_comm_hearings()
-        # for hearing in senate_comm_hearings:
-        #     cid = get_cid(dddb, 'Senate', hearing['name'], year)
-        #     if cid is not None:
-        #         hid = insert_hearing(dddb, hearing['date'], year, cid)
-        #
-        #         if hid is not None and cid is not None:
-        #             insert_comm_hearing(dddb, cid, hid)
-        #
-        #         bills = scrape_senate_agenda(dddb, hearing['url'])
-        #         if hid is not None and len(bills) > 0:
-        #             for bid in bills:
-        #                 insert_hearing_agenda(dddb, hid, bid)
-
-    LOG = {'tables': [{'state': 'NY', 'name': 'Hearing', 'inserted':I_H, 'updated': 0, 'deleted': 0},
-      {'state': 'NY', 'name': 'CommitteeHearings', 'inserted':I_CH, 'updated': 0, 'deleted': 0},
-      {'state': 'NY', 'name': 'HearingAgenda', 'inserted':I_HA, 'updated': U_HA, 'deleted': 0}]}
-    sys.stdout.write(json.dumps(LOG))
-    logger.info(LOG)
-=======
     with connect("live") as dddb:
         logger = create_logger()
         parser = NYHearingParser(get_session_year(dddb, "NY", logger))
@@ -64,7 +20,6 @@ def main():
         hearing_manager = Hearings_Manager(dddb, "NY", logger)
         hearing_manager.import_hearings(hearings, datetime.today().date())
         hearing_manager.log()
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
     main()
