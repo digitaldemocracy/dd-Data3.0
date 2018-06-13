48b882861d953512493d1a5e8d54a65f1746b0e9
Andrew Rose <paro42796@gmail.com>
2018-06-13 13:59:38 -0700

    Final senior project research queries

--- Research/roseResearchQueries.sql
+++ Research/roseResearchQueries.sql
@@ -6,6 +6,8 @@ from (select * from Term where state = 'CA'
      (select * from Bill where state = 'CA'
       and sessionYear = 2015) as Bills2015;
 
+select * from BillSponsors;
+
 # flag for author of bill
 select pid, bid, if(exists(select * from BillSponsors bs
               where bs.pid = Legs2015.pid
@@ -26,17 +28,12 @@ from (select * from Term where state = 'CA'
 SELECT distinct contribution from BillSponsors;
 
 # how many votes did a legislator take on a bill
-select pid, bid, if(exists(select count(*) as voteCounts
+select pid, bid, (select count(*)
                     from BillVoteDetail bvd join BillVoteSummary bvs
                     on bvd.voteId = bvs.voteId
                       where bvd.pid = Legs2015.pid
                       and bvs.bid = Bills2015.bid
-                    group by bvd.pid, bvs.bid), (select count(*)
-                    from BillVoteDetail bvd join BillVoteSummary bvs
-                    on bvd.voteId = bvs.voteId
-                      where bvd.pid = Legs2015.pid
-                      and bvs.bid = Bills2015.bid
-                    group by bvd.pid, bvs.bid), 0) as voteCount
+                    group by bvd.pid, bvs.bid) as voteCount
 from (select * from Term where state = 'CA'
       and year = 2015) as Legs2015,
      (select * from Bill where state = 'CA'
@@ -96,6 +93,47 @@ from (select * from Term where state = 'CA'
      (select * from Bill where state = 'CA'
       and sessionYear = 2015) as Bills2015;
 
+
+# Actual BD count queries
+# All BDs
+select pid, bid, count(*) as bdCount_all
+from Utterance u
+  join BillDiscussion bd on u.did = bd.did
+  join Hearing h on bd.hid = h.hid
+  join CommitteeHearings ch on h.hid = ch.hid
+where pid in (select pid from Term where state = 'CA'
+      and year = 2015)
+and bid in (select bid from Bill where state = 'CA'
+      and sessionYear = 2015)
+GROUP BY u.pid, bd.bid;
+
+# Where legislator is committee member
+select pid, bid, count(*) as bdCount_comm
+from Utterance u
+  join BillDiscussion bd on u.did = bd.did
+  join Hearing h on bd.hid = h.hid
+  join CommitteeHearings ch on h.hid = ch.hid
+where pid in (select pid from Term where state = 'CA'
+      and year = 2015)
+and bid in (select bid from Bill where state = 'CA'
+      and sessionYear = 2015)
+and ch.cid in (select cid from servesOn where pid in (select pid from Term where state = 'CA'
+      and year = 2015))
+GROUP BY u.pid, bd.bid;
+
+# Where legislator is not a committee member
+select pid, bid, count(*) as bdCount_no_comm
+from Utterance u
+  join BillDiscussion bd on u.did = bd.did
+  join Hearing h on bd.hid = h.hid
+  join CommitteeHearings ch on h.hid = ch.hid
+where pid in (select pid from Term where state = 'CA'
+      and year = 2015)
+and bid in (select bid from Bill where state = 'CA'
+      and sessionYear = 2015)
+and ch.cid not in (select cid from servesOn where pid in (select pid from Term where state = 'CA'
+      and year = 2015))
+GROUP BY u.pid, bd.bid;
 
 # Number of words said on a bill
 # wordCount_all: Total word count from all bill discussions
@@ -157,6 +195,15 @@ from (select * from Term where state = 'CA'
      (select * from Bill where state = 'CA'
       and sessionYear = 2015) as Bills2015;
 
+# Actual word count query
+select pid, bid, sum(wordCount) as wordCount from
+(select pid, bid, length(text) - length(replace(text, ' ', '')) as wordCount
+from currentUtterance u join BillDiscussion bd on u.did = bd.did
+where pid in (select pid from Term where state = 'CA'
+      and year = 2015)
+and bid in (select bid from Bill where state = 'CA'
+      and sessionYear = 2015)) as wordCounts
+group by pid, bid;
 
 # Total duration of utterances on a certain bill
 # utteranceDuration_all: Total duration of all utterances on bill
@@ -210,10 +257,6 @@ from (select pid from Term where state = 'CA'
      (select bid from Bill where state = 'CA'
       and sessionYear = 2015) as Bills2015;
 
-select * from currentUtterance u
-join BillDiscussion bd on bd.did = u.did
-where u.pid = 1 and bd.bid = 'CA_201520160AB1018';
-
 select Legs2015.pid, Bills2015.bid,
   CASE WHEN utteranceTimes.times IS NULL THEN 0 ELSE utteranceTimes.times END
     as utteranceDuration_all
@@ -226,3 +269,91 @@ from currentUtterance u
 join BillDiscussion bd on u.did = bd.did
 group by pid, bid) as utteranceTimes on utteranceTimes.pid = pid
                                     and utteranceTimes.bid = Bills2015.bid;
+
+
+# Utterance duration all
+select pid, bid, sum(u.endTime - u.time) as utteranceDuration_all
+      from currentUtterance u
+      join BillDiscussion bd on u.did = bd.did
+      where u.pid in (select pid from Term where state = 'CA'
+      and year = 2015)
+      and bd.bid in (select bid from Bill where state = 'CA'
+      and sessionYear = 2015)
+      group by u.pid, bd.bid;
+
+# Utterance duration comm
+select pid, bid, sum(u.endTime - u.time) as utteranceDuration_comm
+      from currentUtterance u
+      join BillDiscussion bd on u.did = bd.did
+      join Hearing h on bd.hid = h.hid
+      join CommitteeHearings ch on h.hid = ch.hid
+      where u.pid in (select pid from Term where state = 'CA'
+      and year = 2015)
+      and bd.bid in (select bid from Bill where state = 'CA'
+      and sessionYear = 2015)
+      and ch.cid in (select cid from servesOn where pid = u.pid)
+      group by u.pid, bd.bid;
+
+# Utterance duration not comm
+select pid, bid, sum(u.endTime - u.time) as utteranceDuration_not_comm
+      from currentUtterance u
+      join BillDiscussion bd on u.did = bd.did
+      join Hearing h on bd.hid = h.hid
+      join CommitteeHearings ch on h.hid = ch.hid
+      where u.pid in (select pid from Term where state = 'CA'
+      and year = 2015)
+      and bd.bid in (select bid from Bill where state = 'CA'
+      and sessionYear = 2015)
+      and ch.cid not in (select cid from servesOn where pid = u.pid)
+      group by u.pid, bd.bid;
+
+# Vote Count Aye
+select pid, bid, count(bvd.voteId) as ayeVotes
+from BillVoteDetail bvd join BillVoteSummary bvs
+on bvd.voteId = bvs.voteId
+where bvd.result = 'AYE'
+and pid in (select pid from Term where state = 'CA'
+            and year = 2015)
+and bid in (select bid from Bill where state = 'CA'
+            and sessionYear = 2015)
+group by pid, bid;
+
+# Vote Count Nae
+select pid, bid, count(bvd.voteId) as naeVotes
+from BillVoteDetail bvd join BillVoteSummary bvs
+on bvd.voteId = bvs.voteId
+where bvd.result = 'NOE'
+and pid in (select pid from Term where state = 'CA'
+            and year = 2015)
+and bid in (select bid from Bill where state = 'CA'
+            and sessionYear = 2015)
+group by pid, bid;
+
+# Vote Count Abstain
+select pid, bid, count(bvd.voteId) as abstainVotes
+from BillVoteDetail bvd join BillVoteSummary bvs
+on bvd.voteId = bvs.voteId
+where bvd.result = 'ABS'
+and pid in (select pid from Term where state = 'CA'
+            and year = 2015)
+and bid in (select bid from Bill where state = 'CA'
+            and sessionYear = 2015)
+group by pid, bid;
+
+# Unanimous Flag
+select pid, bid, if(exists(select voteId from BillVoteSummary b2
+                where bid = Bills2015.bid
+                and naes != 0), 0, 1) as is_unanimous
+from
+  (select * from Bill where state = 'CA' and sessionYear = 2015) Bills2015,
+  (select * from Term where state = 'CA' and year = 2015) Legs2015;
+
+
+# Get a bill's final subject
+select b.bid, bv.subject from Bill b
+left join BillVersion bv on b.bid = bv.bid
+join (select bv.bid, max(bv.date) as maxDate
+from BillVersion bv
+group by bv.bid) maxDate on bv.date = maxDate.maxDate and bv.bid=maxDate.bid
+where b.sessionYear = 2015
+and b.state = 'CA';
