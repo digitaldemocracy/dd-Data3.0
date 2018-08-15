10ec2e70aefa679d0aa052c6269a1bfb99e407bb
tmgerrit <tmgerrit@calpoly.edu>
2018-08-15 12:19:02 -0700

    Fixed ca_import_vote crash problem

--- CurrentScripts/Utils/Bill_Insertion_Manager.py
+++ CurrentScripts/Utils/Bill_Insertion_Manager.py
@@ -344,13 +344,17 @@ class BillInsertionManager(object):
         """
         ret_val = True
         for detail in vote_detail_list:
-            if detail.name.upper() != 'VACANT' and detail.name.split()[0].upper() != 'VACANT-':
+            # print(str(detail.__dict__))
+            if detail.pid is not None:
                 if not self.is_bill_vote_detail_in_db(detail.__dict__):
                     if not self.insert_bill_vote_detail(detail.__dict__):
                         self.logger.exception(format_logger_message("Unable to insert vote record", detail.__dict__))
                         ret_val = False
                     else:
                         self.BVD_INSERTED += 1
+            else:
+                self.logger.exception(format_logger_message("PID missing from vote", detail.__dict__))
+                ret_val = False
 
         return ret_val
 
